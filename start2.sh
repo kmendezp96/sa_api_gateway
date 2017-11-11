02569b52a9bb7fd161fc672f46e562d71c170dc0
Kevin Mendez <kmendezp@unal.edu.co>
2017-11-10 22:14:09 -0500

    4

--- start2.sh
+++ start2.sh
@@ -1,7 +1,7 @@
 ./rancher-compose --project-name sa-api-gateway \
 --url http://192.168.99.100:8080/v1/projects/1a5 \
---access-key 72D1E5117338928BCE87 \
---secret-key 7tuAwA6YjKVNqPWi2JUoGivUzabXM3fvXn6ZLxmY \
+--access-key 70EE95E3F0704CB1BC85 \
+--secret-key UbkJvYrYRBub9fsXxmzAP8GF2QVtmE4MsyeuFEBw \
 -f docker-compose.yml \
 --verbose up \
 -d --force-upgrade \
